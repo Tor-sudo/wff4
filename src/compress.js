f96f93bc4f3151cc6358801b10020e00dbf88166
Tor-sudo <fhhhjgh567@tutamail.com>
2024-08-29 15:59:56 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -1,29 +1,29 @@
-"use strict";
 const sharp = require('sharp');
 const redirect = require('./redirect');
 
 function compress(req, res, input) {
   const format = req.params.webp ? 'webp' : 'jpeg';
 
-  const transform = sharp()
-    .grayscale(req.params.grayscale)
-    .toFormat(format, {
-      quality: req.params.quality,
-      progressive: true,
-      optimizeScans: true
-    });
+  input.pipe(
+    sharp()
+      .grayscale(req.params.grayscale)
+      .toFormat(format, {
+        quality: req.params.quality,
+        progressive: true,
+        optimizeScans: true
+      })
+      .toBuffer((err, output, info) => {
+        if (err || !info || res.headersSent) return redirect(req, res)
 
-  input.pipe(transform)
-    .on('info', info => {
-      res.setHeader('content-type', `image/${format}`);
-      res.setHeader('x-original-size', req.params.originSize);
-      res.setHeader('x-bytes-saved', req.params.originSize - info.size);
-      res.status(200);
-      res.write(output);
-      res.end();
-    })
-    .on('error', () => redirect(req, res))
-    .pipe(res);
+      res.setHeader('content-type', `image/${format}`)
+        res.setHeader('content-length', info.size)
+        res.setHeader('x-original-size', req.params.originSize)
+        res.setHeader('x-bytes-saved', req.params.originSize - info.size)
+        res.status(200)
+        res.write(output);
+        res.end();
+      })
+    );
 }
 
 module.exports = compress;
